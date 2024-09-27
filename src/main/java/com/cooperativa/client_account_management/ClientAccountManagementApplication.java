71214a105253181dbff857b61c2d78be0856b1f1
Duvan Felipe Correa Ardila <duvan.correa@confiar.coop>
2024-09-27 08:49:01 -0500

    Initialize project structure
    - Create initial folders and files (src, README.md, .gitignore)
    - Set up basic project configuration
    - Add default templates for documentation

---
+++ src/main/java/com/cooperativa/client_account_management/ClientAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cooperativa.client_account_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAccountManagementApplication.class, args);
+	}
+
+}
